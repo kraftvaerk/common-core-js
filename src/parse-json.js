153acf50e7c2db257bd87938476c3f6ab67e4e97
Mithun Dhiman <mi2oon@gmail.com>
2019-04-10 09:35:25 +0200

    first utils added (test)

---
+++ src/parse-json.js
@@ -0,0 +1,4 @@
+// parse a json string to a js object
+const parseJSON = (json) => JSON.parse(json);
+
+export default parseJSON;
---
+++ src/stringify.js
@@ -0,0 +1,4 @@
+// convert an object to a json string
+const stringify = (object) => JSON.stringify(object);
+
+export default stringify;
