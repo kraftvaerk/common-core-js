34baaa1d5814237a12421a22e35e8a39af87e661
Mithun Dhiman <mi2oon@gmail.com>
2019-05-27 16:27:44 +0200

    Storage converted to Class to support local/session

--- src/browser/storage/index.js
+++ src/browser/storage/index.js
@@ -1,36 +1,21 @@
-import stringify from '../../utility/stringify';
-import parseJSON from '../../utility/parse-json';
-
-const type = {
-    local: 'localStorage',
-    session: 'sessionStorage'
-};
-
-const core = {
-    get: (type, id, def /* default value */) => {
-        const result = self[type].getItem(id);
-        return parseJSON(result) || def;
-    },
-    set: (type, id, value) => {
-        const result = stringify(value);
-        self[type].setItem(id, result);
-    },
-    del: (type, id) => {
-        self[type].removeItem(id);
+class Storage {
+    constructor(storage) {
+        this._storage = storage;
     }
-};
 
-export const local = {
-    get: (id, def /* default value */) => { return core.get(type.local, id, def); },
-    set: (id, value) => { core.set(type.local, id, value); },
-    del: (id) => { core.del(type.local, id); }
-};
+    get(id) {
+        return JSON.parse(this._storage.getItem(id));
+    }
+    set(id, value){
+        this._storage.setItem(id, JSON.stringify(value));
+    }
+    delete(id){
+        this._storage.removeItem(id);
+    }
+}
 
-export const session = {
-    get: (id, def /* default value */) => { return core.get(type.session, id, def); },
-    set: (id, value) => { core.set(type.session, id, value); },
-    del: (id) => { core.del(type.session, id); }
-};
+const local = new Storage(window.localStorage);
+const session = new Storage(window.sessionStorage);
 
 export default {
     local,
